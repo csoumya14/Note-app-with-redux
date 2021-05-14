9acf695650a20d17dee90a76bd295384aa35cba3
csoumya14 <soumyachalakkal@gmail.com>
2021-05-14 12:06:50 +0200

    communicating with server added

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App';
-import noteReducer from './reducers/noteReducer';
+import store from './store';
 
-const store = createStore(noteReducer);
-
-const renderApp = () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('root'),
-  );
-};
-
-renderApp();
-store.subscribe(renderApp);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,7 +1,24 @@
+import noteService from '../services/notes';
+
+const initialState = [
+  {
+    content: 'reducer defines how redux store works',
+    important: true,
+    id: 1,
+  },
+  {
+    content: 'state of store can contain any note',
+    important: false,
+    id: 2,
+  },
+];
+
 const noteReducer = (state = [], action) => {
   switch (action.type) {
     case 'NEW_NOTE':
       return [...state, action.data];
+    case 'INIT_NOTES':
+      return action.data;
     case 'TOGGLE_IMPORTANCE': {
       const id = action.data.id;
       const noteToChange = state.find(n => n.id === id);
@@ -17,16 +34,23 @@ const noteReducer = (state = [], action) => {
   }
 };
 
-const generateId = () => Math.floor(Math.random() * 1000000);
-
 export const createNote = content => {
-  return {
-    type: 'NEW_NOTE',
-    data: {
-      content,
-      important: false,
-      id: generateId(),
-    },
+  return async dispatch => {
+    const newNote = await noteService.createNew(content);
+    dispatch({
+      type: 'NEW_NOTE',
+      data: newNote,
+    });
+  };
+};
+
+export const initializeNotes = () => {
+  return async dispatch => {
+    const notes = await noteService.getAll();
+    dispatch({
+      type: 'INIT_NOTES',
+      data: notes,
+    });
   };
 };
 
